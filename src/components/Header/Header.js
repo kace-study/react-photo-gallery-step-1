2f5da493aa05468200d644f12a2c0ba20d30e825
kace-study <kace.study@gmail.com>
2018-11-02 18:22:43 +0900

    step1

---
+++ src/components/Header/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Container } from 'reactstrap';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+  render() {
+    return (
+      <div>
+        {/*  */}
+        <Navbar fixed="top" color="dark" dark>
+          <Container>
+            <NavbarBrand href="/" className="mr-auto">Kace Gallery</NavbarBrand>
+            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+            <Collapse isOpen={!this.state.collapsed} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink href="/components/">Twitter</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Header;
